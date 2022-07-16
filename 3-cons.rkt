ba00edc2320e2f6d0529420bac4cc96d8821a3de
Jimmy Lin <b00502013@gmail.com>
2022-07-17 03:15:08 +0800

    Update 3-cons.rkt

--- 3-cons.rkt
+++ 3-cons.rkt
@@ -1,4 +1,5 @@
 #lang racket
+
 (define cal (lambda (op a b) (
   cond
   ((eq? op `+) (+ a b))
@@ -7,20 +8,45 @@
   ((eq? op `/) (/ a b))
   (else (error "Unknown operator" op))
 )))
+
 (cal `+ 1 2)
 (cal `- 1 2)
 (cal `* 1 2)
 (cal `/ 1 2)
+
 (define rember (lambda (l a) (
   cond
+  ((null? l) `())
   ((eq? (car l) a) (cdr l))
   (else (
-    let (
-      (head (car l))
-      (tail (rember (cdr l) a))
-    ) (cons head tail)
+    cons
+    (car l)
+    (rember (cdr l) a)
   ))
 )))
+
 (rember `(foo bar) `foo)
 (rember `(foo bar) `bar)
 (rember `(foo bar baz) `bar)
+(rember `() `bar)
+
+(define first (lambda (l) (
+  cond
+  ((null? l) `())
+  (else (car l))
+)))
+
+(first `(a b c))
+(first `())
+
+(define firsts (lambda (l) (
+  cond
+  ((null? l) `())
+  (else (
+    cons
+    (first (car l))
+    (firsts (cdr l))
+  ))
+)))
+
+(firsts `((a b c) (1 2 3) (x y z)))
