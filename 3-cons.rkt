2a6fdde8259f1511d8e29f2afff93d569f9724d2
Jimmy Lin <b00502013@gmail.com>
2022-07-19 01:45:53 +0800

    Update 3-cons.rkt

--- 3-cons.rkt
+++ 3-cons.rkt
@@ -50,3 +50,66 @@
 )))
 
 (firsts `((a b c) (1 2 3) (x y z)))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat)
+        `())
+      ((eq? (car lat) old)
+        (cons old (cons new (cdr lat))))
+      (else
+        (cons (car lat) (insertR new old (cdr lat)))))))
+
+(insertR `z `b `(c c c c b a))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat)
+        `())
+      ((eq? (car lat) old)
+        (cons new lat))
+      (else
+        (cons (car lat) (insertL new old (cdr lat)))))))
+
+(insertL `z `b `(c c c c b a))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat)
+        `())
+      ((eq? (car lat) old)
+        (cons new (cdr lat)))
+      (else
+        (cons (car lat) (subst new old (cdr lat)))))))
+
+(subst `z `b `(c c c c b a))
+
+(define include?
+  (lambda (lat a)
+  (cond
+    ((null? lat)
+      #f)
+    ((eq? (car lat) a)
+      #t)
+    (else
+      (include? (cdr lat) a)))))
+
+(include? `(a b) `a)
+(include? `(a b c) `b)
+(include? `(a b c) `d)
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat)
+        `())
+      ((include? (list o1 o2) (car lat))
+        (cons new (cdr lat)))
+      (else
+        (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))
+
+(subst2 `z `x `b `(c c c c b a))
+(subst2 `z `c `b `(c c c c b a))
