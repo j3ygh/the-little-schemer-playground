17a05cec9aead81435316621df6b20067a2b1fc6
Jimmy Lin <b00502013@gmail.com>
2022-07-20 21:27:56 +0800

    Update 5-stars.rkt

--- 5-stars.rkt
+++ 5-stars.rkt
@@ -3,11 +3,11 @@
 (define rember*
   (lambda (l a) 
     (cond
+      ((null? l)
+        `())
       ((list? (car l))
         (cons (rember* (car l) a)
           (rember* (cdr l) a)))
-      ((null? l)
-        `())
       ((eq? (car l) a)
         (cdr l))
       (else
@@ -15,13 +15,25 @@
           (rember* (cdr l) a))))))
 
 (rember* `((foo bar) foo bar baz baz foo) `bar)
+(rember* `(() foo bar baz baz foo) `bar)
+(rember* `() `bar)
 
 (define insertR*
   (lambda (new old l)
     (cond
-      ((pair? l)
-        `())
       ((null? l)
         `())
+      ((list? (car l))
+        (cons (insertR* new old (car l))
+          (insertR* new old (cdr l))))
+      ((eq? (car l) old)
+        (cons old
+          (cons new
+            (insertR* new old (cdr l)))))
       (else
-        `()))))
+        (cons (car l)
+          (insertR* new old (cdr l)))))))
+
+(insertR* 8 `bar `((foo bar) foo bar baz baz foo))
+(insertR* 8 `bar `(() foo bar baz baz foo))
+(insertR* 8 `bar `())
