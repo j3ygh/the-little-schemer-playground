ac36443f3a8f080aea301088cd09068fe152368b
Jimmy Lin <b00502013@gmail.com>
2022-07-20 16:41:54 +0800

    Update 5-stars.rkt

--- 5-stars.rkt
+++ 5-stars.rkt
@@ -1,1 +1,23 @@
 #lang racket
+
+(define atom?
+  (lambda (x)
+    (and
+      (not (pair? x))
+      (not (null? x)))))
+
+(define rember
+  (lambda (l a) 
+    (cond
+      ((null? l)
+        `())
+      ((eq? (car l) a)
+        (cdr l))
+      (else
+        (cons (car l)
+          (rember (cdr l) a))))))
+
+(let
+  ((a `sauce)
+  (l `(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce))))
+    (list a l))
