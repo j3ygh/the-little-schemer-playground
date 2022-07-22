2e88be4f004bc3d5af7f26a8adc788294659ea90
Jimmy Lin <b00502013@gmail.com>
2022-07-23 01:55:08 +0800

    Update 6-shadows.rkt

--- 6-shadows.rkt
+++ 6-shadows.rkt
@@ -73,3 +73,96 @@
 ; Recur on the subparts that are of the same nature:
 ; • On the sublists of a list.
 ; • On the subexpressions of an arithmetic expression. 
+
+( define 1st-sub-aexp
+  ( lambda ( aexp )
+    ( car ( cdr aexp ) ) ) )
+
+( define 2st-sub-aexp
+  ( lambda ( aexp )
+    ( car ( cdr ( cdr aexp ) ) ) ) )
+
+( define operator
+  ( lambda ( aexp )
+    ( car aexp ) ) )
+
+( define value2
+  ( lambda ( exp )
+    ( cond
+      ( ( atom? exp )
+        exp )
+      ( ( eq? ( operator exp ) `+ )
+        ( +
+          ( value2 ( 1st-sub-aexp exp ) )
+          ( value2 ( 2st-sub-aexp exp ) ) ) )
+      ( ( eq? ( operator exp ) `- )
+        ( -
+          ( value2 ( 1st-sub-aexp exp ) )
+          ( value2 ( 2st-sub-aexp exp ) ) ) )
+      ( ( eq? ( operator exp ) `* )
+        ( *
+          ( value2 ( 1st-sub-aexp exp ) )
+          ( value2 ( 2st-sub-aexp exp ) ) ) )
+      ( ( eq? ( operator exp ) `/ )
+        ( /
+          ( value2 ( 1st-sub-aexp exp ) )
+          ( value2 ( 2st-sub-aexp exp ) ) ) ) ) ) )
+
+( value2 42 )
+( value2 `( + 1 2 ) )
+( value2 `( + 1 ( * 3 4 ) ) )
+
+; The Eighth Commandment
+; Use help functions to abstract from representations.
+
+( define zero?
+  ( lambda ( exp )
+    ( null? exp ) ) )
+
+( zero? `() )
+
+( define add-1
+  ( lambda ( exp )
+    ( cons `() exp ) ) )
+
+( define sub-1
+  ( lambda ( exp )
+    ( cdr exp ) ) )
+
+( add-1 `() )
+( add-1 ( add-1 `() ) )
+( add-1 ( add-1 ( add-1 `() ) ) )
+( add-1 ( sub-1 ( add-1 `() ) ) )
+
+( define plus
+  ( lambda ( n m )
+    ( cond
+      ( ( zero? m )
+        n )
+      ( else
+        ( plus ( add-1 n ) ( sub-1 m ) ) ) ) ) )
+
+( plus `() `(() () () ()) )
+
+; Recall lat ?
+; Do you remember what the value of (lat ? ls)
+; is where ls is (1 2 3)
+; What is (1 2 3) with our new numbers?
+; What is (lat? ls) where
+; ls is ((()) (()(
+; )) (()(
+; )()))
+; Is that bad?
+; Shadows
+; Easy:
+; (define lat ?
+; (lambda (l)
+; (cond
+; ((null? l) #t )
+; (( atom? ( car l)) (lat? ( cdr l)))
+; (else #f ))) )
+; But why did you ask?
+; #t , of course.
+; ((()) (()()) (()()())) .
+; It is very false.
+; You must beware of shadows. 
