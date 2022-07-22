60c77b3df400122d4c1dc3f5ce3a1e7d28629d4d
Jimmy Lin <b00502013@gmail.com>
2022-07-23 01:05:44 +0800

    Update 6-shadows.rkt

--- 6-shadows.rkt
+++ 6-shadows.rkt
@@ -1,5 +1,5 @@
-; Try a new indentation style in this file.
 #lang racket
+; Try a new indentation style in this file.
 
 ; Helpers
 ( define operators `( + - * / ) )
@@ -18,24 +18,27 @@
   ( lambda ( exp )
     ( in? exp operators ) ) )
 
+( define atom? 
+  ( lambda ( exp )
+    ( and
+      ( not ( null? exp ) )
+      ( not ( pair? exp ) ) ) ) )
+
 ; Functions
 ( define numbered?
   ( lambda ( exp )
     ( cond
-      ( ( null? exp )
-        #f )
-      ( ( list? exp )
+      ( atom?
+        ( number? exp ) )
+      ( else
         ( and
           ( operator? ( car exp ) )
           ( numbered? ( car ( cdr exp ) ) )
-          ( numbered? ( car ( cdr ( cdr exp ) ) ) ) ) )
-      ( ( number? exp )
-        #t )
-      ( else
-        `foo ) ) ) )
+          ( numbered? ( car ( cdr ( cdr exp ) ) ) ) ) ) ) ) )
 
 ; Testings
 ( numbered? 42 )
+( numbered? `foo )
 ( numbered? `() )
 ( numbered? `( + 1 2 ) )
 ( numbered? `( + 1 ( * 3 4 ) ) )
